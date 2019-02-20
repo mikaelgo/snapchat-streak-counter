7a849b9335caa81fe7973961b7d66fbf51e796de
mikaelgo <mikaelgo@hotmail.com>
2019-02-20 17:37:16 +0200

    formatted app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import  React from 'react';
-import Title from './components/Title';
-import Form from './components/Form';
-import Streak from './components/Streak';
+import React from "react";
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Streak from "./components/Streak";
 
 class App extends React.Component {
-
   state = {
     streak: undefined,
     error: undefined
   };
 
-  
-
-  calculateStreak = (e) => {
+  calculateStreak = e => {
     e.preventDefault();
 
     let date = new Date();
     console.log("DATE BEFORE SETSTATE: " + date);
     const streakValue = e.target.elements.streak.value;
     const streakNumber = Number(streakValue);
-    console.log("STREAK: "+streakNumber);
-    date.setDate(date.getDate()-streakNumber);
+    console.log("STREAK: " + streakNumber);
+    date.setDate(date.getDate() - streakNumber);
     const formattedDate = new Intl.DateTimeFormat().format(date);
     const streakDateString = String(formattedDate);
 
-    console.log("DATE: "+ date);
-    
-
+    console.log("DATE: " + date);
 
     if (streakValue) {
       this.setState({
@@ -37,23 +32,18 @@ class App extends React.Component {
       this.setState({
         streak: undefined,
         error: "Please enter the correct values!"
-    });
-  
-  }
-  }
+      });
+    }
+  };
 
   render() {
     return (
-      
       <div>
-        <Title/>
-        <Form calculateStreak = {this.calculateStreak}/>
-        <Streak streak = {this.state.streak}/>
-        
+        <Title />
+        <Form calculateStreak={this.calculateStreak} />
+        <Streak streak={this.state.streak} />
       </div>
-      
     );
-    
   }
 }
 
